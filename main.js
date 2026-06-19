9febeb0fe10bcbb8529205b9a781539aa1990b23
yangtuoqiao <18624138284@163.com>
2026-06-19 20:56:00 +0800

    sou

--- main.js
+++ main.js
@@ -33,3 +33,39 @@ function renderBooks(list) {
     </div>
   `).join("");
 }
+document.addEventListener("DOMContentLoaded", () => {
+
+  let books = [];
+
+  fetch("books.json")
+    .then(res => res.json())
+    .then(data => {
+      books = data;
+      renderBooks(books);
+    });
+
+  const input = document.getElementById("searchInput");
+
+  input.addEventListener("input", function () {
+
+    const keyword = this.value.toLowerCase();
+
+    const filtered = books.filter(book =>
+      book.title.toLowerCase().includes(keyword)
+    );
+
+    renderBooks(filtered);
+  });
+
+  function renderBooks(list) {
+    const container = document.getElementById("list");
+
+    container.innerHTML = list.map(book => `
+      <div class="book">
+        <span>${book.title}</span>
+        <a href="${book.file}" download>下载</a>
+      </div>
+    `).join("");
+  }
+
+});
